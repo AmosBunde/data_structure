6efe5fd8729272e4f956ddfea8c587109f3257b8
AmosBunde <git.bunde@hotmail.com>
2023-08-15 08:48:56 +0300

    getting inventory updated

--- inventory.py
+++ inventory.py
@@ -2,9 +2,11 @@ from collections import Counter
 
 def main():
 
-    inventory = Counter(STAA001= 20, SAL002= 50, ENT004=60)
-    sales = Counter(STAA001= 10, SAL002= 34, ENT004=20)
+    inventory = Counter(STAA001=20, SAL002=50, ENT004=60)
+    sales = Counter(STAA001=10, SAL002=34, ENT004=20)
     inventory = inventory - sales
+    new_inventory = {"STAOO1": 35, "SAL002":35, "ENT004": 23}
+    inventory.update(new_inventory)
 
     print(inventory)
 
