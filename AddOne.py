f50c228c6967dcfac1d93cbd334968755b7fd316
AmosBunde <git.bunde@hotmail.com>
2023-08-19 15:33:51 +0300

    square and addition

---
+++ AddOne.py
@@ -0,0 +1,12 @@
+def addOne(anInteger):
+    anInteger = anInteger +1
+    return anInteger
+
+def main():
+    anInteger = 53
+    print ("anInteger:", anInteger)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ squareInteger.py
@@ -0,0 +1,12 @@
+def squareInteger(anInteger):
+    anInteger = anInteger ** 2
+    return anInteger
+
+def main():
+    anInteger = 57
+    print("Square is:", squareInteger(anInteger))
+
+
+if __name__== "__main__":
+    main()
+
