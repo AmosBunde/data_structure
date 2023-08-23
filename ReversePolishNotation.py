a70c0013189a53b822458e17239176d2bc1701cf
AmosBunde <git.bunde@hotmail.com>
2023-08-23 06:29:37 +0300

    Reverse Polish Notation

---
+++ ReversePolishNotation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def evalRPN(self, token: list[str]) -> None:
+        stack = []
+        for c in token:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
